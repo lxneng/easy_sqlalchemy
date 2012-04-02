60018fd3d7450f7d4a2c35bfc8e53b1a53ba6dcc
Eric Lo <lxneng@gmail.com>
2012-04-02 22:53:13 +0800

    add UUID and INET types

---
+++ src/easy_sqlalchemy/types.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import uuid
+import IPy
+from sqlalchemy.types import TypeDecorator
+from sqlalchemy.types import CHAR
+from sqlalchemy.dialects import postgresql
+
+
+# GUID type from:
+# https://github.com/2style4you/s4u.sqlalchemy/blob/master/src/s4u/sqlalchemy/uuid.py
+class GUID(TypeDecorator):
+    """Platform-independent GUID type.
+
+    Uses Postgresql's UUID type, otherwise uses CHAR(36), storing as
+    stringified hex values.
+
+    This implementation is based on the SQLAlchemy
+    `backend-agnostic GUID Type
+    <http://www.sqlalchemy.org/docs/core/types.html#backend-agnostic-guid-type>`_
+    example.
+    """
+    impl = CHAR
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == 'postgresql':
+            return dialect.type_descriptor(postgresql.UUID())
+        else:
+            return dialect.type_descriptor(CHAR(36))
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        elif dialect.name == 'postgresql':
+            return str(value)
+        else:
+            if not isinstance(value, uuid.UUID):
+                return str(uuid.UUID(value))
+            else:
+                # hexstring
+                return str(value)
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            return uuid.UUID(value)
+
+
+class INET(TypeDecorator):
+    """Platform-independent INET type.
+
+    This type stores IPv4 and IPv6 addresses and netmarks. This type
+    uses the `IPy.IP <http://pypi.python.org/pypi/IPy>`_ instances
+    to store values. As input it will accept also a standard python
+    string.
+
+    When using a PostgreSQL backend the native INET type is used. On
+    other databases values are stored as CHAR(42).
+    """
+    impl = CHAR
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == 'postgresql':
+            return dialect.type_descriptor(postgresql.INET())
+        else:
+            return dialect.type_descriptor(CHAR(42))
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        elif dialect.name == 'postgresql':
+            return str(value)
+        else:
+            if not isinstance(value, IPy.IP):
+                value = IPy.IP(value)
+            return str(value)
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            return IPy.IP(value)
