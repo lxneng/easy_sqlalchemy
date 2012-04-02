76002b6e70dd17edd7b0c1d80ae67a6e861eb711
Eric Lo <lxneng@gmail.com>
2012-04-02 18:47:14 +0800

    add __todict__, __iter__ and fromdict function to object

--- src/easy_sqlalchemy/meta.py
+++ src/easy_sqlalchemy/meta.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 from sqlalchemy import schema
 from sqlalchemy.ext.declarative import declarative_base
+from easy_sqlalchemy.ext import (
+        todict,
+        iterfunc,
+        fromdict
+        )
+
 
 Session = None
 
 metadata = schema.MetaData()
 
 BaseObject = declarative_base(metadata=metadata)
+BaseObject.__todict__ = todict
+BaseObject.__iter__ = iterfunc
+BaseObject.fromdict = fromdict
+
 
 __all__ = ['Session', 'metadata', 'BaseObject']
